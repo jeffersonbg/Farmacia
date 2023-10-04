dfc07905f255fd085d239ec387085d5d3ae60438
Arthur Abreu <100291563+ArtthurAbreu@users.noreply.github.com>
2023-10-04 01:31:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+function logar(){
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+   
+    if (email == "arthur@hotmail.com" && password == 12345) {
+        
+        window.location.href = "cadastro.html" ;
+
+    } else {
+        alert("Credenciais inválidas. Tente novamente.");
+    }
+}
+
+function cadastrar(){
+    
+}
